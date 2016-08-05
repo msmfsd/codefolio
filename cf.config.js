767dba2c2f9de0b3c8e6250a1e846d6dd5ae17e6
Magnus Morris <magnusmorris@icloud.com>
2016-08-05 12:57:54 +1000

    travis build fixes

---
+++ cf.config.js
@@ -0,0 +1,12 @@
+const __CONFIG__ = {
+  development: {
+    "API_URL": "http://localhost:8080",
+    "API_KEY": "D6F319D4D224147BDCDBE493E3FDE"
+  },
+  production: {
+    "API_URL": "http://msmfsd.com:8080",
+    "API_KEY": "D6F319D4D224147BDCDBE493E3FDE"
+  }
+}
+
+export default __CONFIG__
