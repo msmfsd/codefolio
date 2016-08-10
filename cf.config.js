e1a442bc9ffefe6a66f5e7b28a07691805b0b1f7
Magnus Morris <magnusmorris@icloud.com>
2016-08-10 17:50:51 +1000

    Config amends

--- cf.config.js
+++ cf.config.js
@@ -4,9 +4,9 @@
  * API_PROD_URL: Your production Node API server, only needed for deployment to production
  */
 const __CONFIG__ = {
-  API_KEY: "D6F319D4D224147BDCDBE493E3FDE",
+  API_KEY: "AAAAABBBBB111112222233333",
   API_DEV_URL: "http://localhost:8080",
-  API_PROD_URL: "http://msmfsd.com:8080"
+  API_PROD_URL: "http://your-production-api-node-server.com:8080"
 }
 
 export default __CONFIG__
